9922ef119f03ef662711cc4319b18efdcd3f40db
Keijiro Takahashi <keijiro@gmail.com>
2018-05-25 11:37:09 +0900

    modifier

--- Assets/FlyAnimationSystem.cs
+++ Assets/FlyAnimationSystem.cs
@@ -13,6 +13,7 @@ public class FlyAnimationSystem : JobComponentSystem
         [ReadOnly] public ComponentDataArray<Fly> Flies;
         [ReadOnly] public ComponentDataArray<Facet> Facets;
         [ReadOnly] public ComponentDataArray<Position> Positions;
+        [ReadOnly] public float Time;
 
         [NativeDisableParallelForRestriction] public NativeArray<float3> Vertices;
         [NativeDisableParallelForRestriction] public NativeArray<float3> Normals;
@@ -26,6 +27,12 @@ public class FlyAnimationSystem : JobComponentSystem
             var v1 = p + f.Vertex1;
             var v2 = p + f.Vertex2;
             var v3 = p + f.Vertex3;
+
+            var offs = new float3(0, 0, Time * 1.6f);
+            v1 *= 1 + noise.cnoise(v1 * 2 + offs) * 0.2f;
+            v2 *= 1 + noise.cnoise(v2 * 2 + offs) * 0.2f;
+            v3 *= 1 + noise.cnoise(v3 * 2 + offs) * 0.2f;
+
             var n = math.normalize(math.cross(v2 - v1, v3 - v1));
 
             Vertices[vi + 0] = v1;
@@ -62,11 +69,12 @@ public class FlyAnimationSystem : JobComponentSystem
                 Flies = _flyGroup.GetComponentDataArray<Fly>(),
                 Facets = _flyGroup.GetComponentDataArray<Facet>(),
                 Positions = _flyGroup.GetComponentDataArray<Position>(),
+                Time = UnityEngine.Time.time,
                 Vertices = renderer.Vertices,
                 Normals = renderer.Normals
             };
 
-            deps = job.Schedule(_flyGroup.CalculateLength(), 64, deps);
+            deps = job.Schedule(_flyGroup.CalculateLength(), 16, deps);
         }
 
         _renderers.Clear();
